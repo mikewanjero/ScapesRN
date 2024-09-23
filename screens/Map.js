14d3bfde1555c2de7da8463fae731ca8e4c5c9a6
mikewanjero <mikewanj@gmail.com>
2024-09-23 11:29:15 +0300

    renaming functions

--- screens/Map.js
+++ screens/Map.js
@@ -1,3 +1,3 @@
-export default function name(params) {
+export default function Map() {
   return <View></View>;
 }
--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,3 +1,3 @@
-export default function name(params) {
+export default function PlaceDetails() {
   return <View></View>;
 }
