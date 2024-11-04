e706e33feb2bc37cc1cf0e66dd4e37d7cf1fe6de
mikewanjero <mikewanj@gmail.com>
2024-11-04 10:13:32 +0300

    Showing an image preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,20 +1,45 @@
-import { Button, View } from "react-native";
+import { Button, View, Image, Text, StyleSheet } from "react-native";
 import { launchCameraAsync } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "./../../constants/colors";
 
 export default function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
   async function takeImageHandler() {
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image.uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
