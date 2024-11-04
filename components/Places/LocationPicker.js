b9997259d352425402fecbef396c93e46243c4df
mikewanjero <mikewanj@gmail.com>
2024-11-04 12:55:49 +0300

    Location Picker

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,40 @@
+import { StyleSheet, View } from "react-native";
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
+export default function LocationPicker() {
+  function getLocationHandler() {}
+
+  function chooseMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon={"location"} onClick={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon={"map"} onClick={chooseMapHandler}>
+          Choose on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
